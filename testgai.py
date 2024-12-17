c5e3b0c616fb70bf28e08feb4e55bb99d859544a
Tzurmar94555 <C112156134@nkust.edu.tw>
2024-12-17 15:07:13 +0800

    asasdfasf

--- testgai.py
+++ testgai.py
@@ -12,3 +12,7 @@ generativeai.configure(api_key="AIzaSyDHEz77ybg7OG_HTO_smM-M_V5hKzWWJPg")
 response = generativeai.GenerativeModel('gemini-2.0-flash-exp').generate_content('妳是誰 ?')
 print(response.text)
 
+
+
+# add image upload
+
