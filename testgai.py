631a899b27626a55a8e968ffa723dcb9ddfabedd
Tzurmar94555 <C112156134@nkust.edu.tw>
2024-12-17 14:52:40 +0800

    asdfasfd

---
+++ testgai.py
@@ -0,0 +1,14 @@
+#import google.generativeai as generativeai
+
+#generativeai.configure(api_key="AIzaSyDHEz77ybg7OG_HTO_smM-M_V5hKzWWJPg")
+#model_id = "gemini-2.0-flash-exp"
+
+
+
+import os
+import google.generativeai as generativeai
+
+generativeai.configure(api_key="AIzaSyDHEz77ybg7OG_HTO_smM-M_V5hKzWWJPg")
+response = generativeai.GenerativeModel('gemini-2.0-flash-exp').generate_content('妳是誰 ?')
+print(response.text)
+
